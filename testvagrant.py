d48e4d0bf85ed8d3fe67c9a9f1d64eb2efecf6e5
sagar373 <113368373+sagar373@users.noreply.github.com>
2023-03-31 17:43:23 +0530

    Update testvagrant.py

--- testvagrant.py
+++ testvagrant.py
@@ -1,49 +1,14 @@
-class HouseholdExpenses:
-    def __init__(self):
-        self.expense_categories = []
-        self.weekly_budget = []
+newspapers=['TOI','Hindu','ET','BM','HT']
+prices=[26,20.5,34,10.5,18]
 
-    def add_category(self, category):
-        self.expense_categories.append(category)
-
-    def add_weekly_budget(self, budget):
-        self.weekly_budget.append(budget)
-
-    def get_expense_categories(self):
-        return self.expense_categories
-
-    def get_weekly_budget(self):
-        return self.weekly_budget
-
-    def calculate_total_expenses(self):
-        total_expenses = 0
-        for category in self.expense_categories:
-            total_expenses += category[1]
-        return total_expenses
-
-    def calculate_weekly_budget(self):
-        total_budget = 0
-        for budget in self.weekly_budget:
-            total_budget += budget[1]
-        return total_budget
-
-# Create a new instance of the HouseholdExpenses class
-household_expenses = HouseholdExpenses()
-
-# Add expense categories
-household_expenses.add_category(["Food", 100])
-household_expenses.add_category(["Utilities", 50])
-household_expenses.add_category(["Entertainment", 25])
-
-# Add weekly budget
-household_expenses.add_weekly_budget(["Food", 200])
-household_expenses.add_weekly_budget(["Utilities", 100])
-household_expenses.add_weekly_budget(["Entertainment", 50])
-
-# Calculate total expenses and weekly budget
-total_expenses = household_expenses.calculate_total_expenses()
-weekly_budget = household_expenses.calculate_weekly_budget()
-
-# Print results
-print("Total Expenses:", total_expenses)
-print("Weekly Budget:", weekly_budget)
+def subscriptions(newspapers,prices,budget):
+    l=len(newspapers)
+    ans=[]
+    for i in range(l):
+        for j in range(i+1,l):
+            if prices[i]+prices[j]<=budget:
+                ans.append({newspapers[i],newspapers[j]})
+    return ans
+    
+budget=int(input("enter weekly budget:"))
+print(subscriptions(newspapers,prices,budget))
